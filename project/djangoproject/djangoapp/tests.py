e43d2359ecfb6691b120410a4a23fb02d086fbad
Trenton Moore <45805884+gotrenton@users.noreply.github.com>
2020-10-25 13:49:13 -0500

    postgres integration

--- project/djangoproject/djangoapp/tests.py
+++ project/djangoproject/djangoapp/tests.py
@@ -1,3 +1,3 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Create your tests here. If you want to do test cases
--- project/djangoproject/djangoproject/settings.py
+++ project/djangoproject/djangoproject/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'djangoapp.apps.DjangoappConfig', #define the django app
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
