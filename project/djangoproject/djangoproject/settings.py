2434358ec862b323aa53a8d7d5fe6ff1a9bfe3f9
Trenton Moore <45805884+gotrenton@users.noreply.github.com>
2020-10-25 09:59:19 -0500

    in process of configuring database connection

--- project/djangoproject/djangoproject/settings.py
+++ project/djangoproject/djangoproject/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',  # replace sqlite3 with postgres
+        'NAME': 'artsdb',  # replace this with our database name
+        'USER': 'postgres',  # add the username(postgres)
+        'PASSWORD': '1234',  # add password
+        'HOST': 'localhost'  # add where it's being hosted
+
     }
 }
 
