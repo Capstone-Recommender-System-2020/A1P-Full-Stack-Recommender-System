ec3872abb05105c97cd8f0dc3c7f8962e6458ff3
Trenton Moore <45805884+gotrenton@users.noreply.github.com>
2020-10-25 14:21:13 -0500

    create a table in postgres using ORM

--- project/djangoproject/djangoapp/models.py
+++ project/djangoproject/djangoapp/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 # Create your models here.
 
-class Destination(models.Model): #convert a class as a model by specifying models.Model
-    
+
+class Destination(models.Model):  # convert a class as a model by specifying models.Model
+
     name = models.CharField(max_length=100)
-    img = models.ImageField(upload_to = 'pics')
+    img = models.ImageField(upload_to='pics')
     desc = models.TextField()
-    price = models.IntegerField
+    price = models.IntegerField()
     offer = models.BooleanField(default=False)
